3a0727304916d6e3729a3bb900eb5efba4ca8bd3
MuhdAddasHamdard <muhdabbaskan@gmail.com>
2026-07-22 10:16:37 +0430

    Implement user routing and controller

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
+const PORT = 5000;
 
 app.use(express.json());
-
-const PORT = 5000;
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Financial Management System API");
